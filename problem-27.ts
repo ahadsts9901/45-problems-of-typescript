724427bc14cb3cf6ed7d29e4f5cb84f4883b4de2
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 11:29:17 +0500

    problem-27

---
+++ problem-27.ts
@@ -0,0 +1,29 @@
+let alien1 = "green"
+
+if (alien1 === "green") {
+    console.log("congrats you earned 5 points");
+} else if (alien1 === "yellow") {
+    console.log("congrats you earned 10 points");
+} else if (alien1 === "red") {
+    console.log("congrats you earned 15 points");
+}
+
+let alien2 = "yellow"
+
+if (alien2 === "green") {
+    console.log("congrats you earned 5 points");
+} else if (alien2 === "yellow") {
+    console.log("congrats you earned 10 points");
+} else if (alien2 === "red") {
+    console.log("congrats you earned 15 points");
+}
+
+let alien3 = "red"
+
+if (alien3 === "green") {
+    console.log("congrats you earned 5 points");
+} else if (alien3 === "yellow") {
+    console.log("congrats you earned 10 points");
+} else if (alien3 === "red") {
+    console.log("congrats you earned 15 points");
+}
