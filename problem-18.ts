28e1bc6bacb4dde8e6431c203d30f2b907d29f88
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 10:42:54 +0500

    problem-18

---
+++ problem-18.ts
@@ -0,0 +1,17 @@
+// an array of countries
+const placesToVisit = ["Pakistan", "Saudi Arab", "Turkey", "USA", "Qatar", "Dubai"]
+
+// prit the array
+console.log(placesToVisit);
+
+// sort the array
+console.log([...placesToVisit].sort());
+
+// is array is still in its original condition
+console.log(placesToVisit);
+
+// sort in descending order
+console.log([...placesToVisit].sort().reverse());
+
+// is array is still in its original condition
+console.log(placesToVisit);
