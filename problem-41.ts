a244957399faa608f2839aab1e0855c9e4350721
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 13:29:51 +0500

    41

---
+++ problem-41.ts
@@ -0,0 +1,13 @@
+const magicians = ["magician-1", "magician-2", "magician-3"]
+
+const show_magicians = (array: string[]) => {
+
+    for (let i = 0; i < array.length; i++) {
+
+        console.log(array[i]);
+
+    }
+
+}
+
+show_magicians(magicians)
