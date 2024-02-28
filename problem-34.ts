d6c5c2c2eef5f2f4a0c389b22b5dc21420ed74da
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 13:03:27 +0500

    34

---
+++ problem-34.ts
@@ -0,0 +1,15 @@
+const foods = ["biryani", "pulao", "tikka"]
+
+for (let i = 0; i < foods.length; i++) {
+
+    console.log(foods[i]);
+
+}
+
+for (let i = 0; i < foods.length; i++) {
+
+    console.log(`I like ${foods[i]}`);
+
+}
+
+console.log("I really like food");
