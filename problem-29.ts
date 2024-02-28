df4926d8cafe1f24cd9bdc9656e410b1b7b02924
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 12:44:05 +0500

    29

---
+++ problem-29.ts
@@ -0,0 +1,13 @@
+let favorit_fruits = ["mango", "apple", "banana"];
+
+if (favorit_fruits.includes("mango")) {
+    console.log("I like mangoes");
+} else if (favorit_fruits.includes("cherry")) {
+    console.log("I like cherries");
+} else if (favorit_fruits.includes("apple")) {
+    console.log("I like apples");
+} else if (favorit_fruits.includes("banana")) {
+    console.log("I like bananas");
+} else {
+    console.log("I like fruits");
+}
