73e43873e61c8252a782cbb800af7f75c71b2252
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 13:11:03 +0500

    36

---
+++ problem-36.ts
@@ -0,0 +1,6 @@
+const make_shirt = (size: number | string, text: string) => {
+    console.log(`the size of the t-shirt is ${size}`);
+    console.log(`the text printed on t-shirt is ${text}`);
+}
+
+make_shirt("xl", "hello world");
