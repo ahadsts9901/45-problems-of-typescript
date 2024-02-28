eb6c602ba8599f071912d780958a6b1ad5ff682c
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 12:41:40 +0500

    problem-28

---
+++ problem-28.ts
@@ -0,0 +1,15 @@
+let age = 35
+
+if (age < 2) {
+    console.log("person is baby");
+} else if (age < 4) {
+    console.log("person is toddler");
+} else if (age < 13) {
+    console.log("person is kid");
+} else if (age < 20) {
+    console.log("person is teenager");
+} else if (age < 65) {
+    console.log("person is adult");
+} else {
+    console.log("person is elder");
+}
