53f3ee1843bb8a2b9d1a014674abe424fae89dc1
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 13:20:24 +0500

    39

---
+++ problem-39.ts
@@ -0,0 +1,7 @@
+const city_country = (city: string, country: string): string => {
+    return `${city} is in ${country}`
+}
+
+city_country("Karachi", "Pakistan")
+city_country("Islamabad", "Pakistan")
+city_country("Lahore", "Pakistan")
