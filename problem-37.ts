19d5fcb9c34124a013351ab0ba7529cc233e0e2b
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 13:13:22 +0500

    37

---
+++ problem-37.ts
@@ -0,0 +1,18 @@
+function large_shirt(size = "large", text = "I love javaScript") {
+    console.log(`the size of the t-shirt is ${size}`);
+    console.log(`the text printed on t-shirt is ${text}`);
+}
+
+function medium_shirt(size = "medium", text = "I love typescript") {
+    console.log(`the size of the t-shirt is ${size}`);
+    console.log(`the text printed on t-shirt is ${text}`);
+}
+
+function extraLarge_shirt(size = "xxl", text = "hello world") {
+    console.log(`the size of the t-shirt is ${size}`);
+    console.log(`the text printed on t-shirt is ${text}`);
+}
+
+large_shirt();
+medium_shirt();
+extraLarge_shirt();
