4e93a6b0befa46d8942f3f987b9c00a0e3c58424
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 13:05:59 +0500

    35

---
+++ problem-35.ts
@@ -0,0 +1,13 @@
+let animals = ["cats", "parrots", "birds"];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(`${animals[i]} would make a great pet`);
+}
+
+console.log(
+    "All of these animals have one common characteristic that they all are cute"
+);
