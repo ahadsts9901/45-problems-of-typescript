2d240fa6af677fe1218d3d76b69a3295f84806de
Muhammad Ahad <ahadsts990@gmail.com>
2024-02-28 11:07:49 +0500

    problem-23

---
+++ problem-23.ts
@@ -0,0 +1,11 @@
+const my_name = "john"
+console.log(`is my name == "john"`);
+console.log(my_name == "john");
+
+const _vehicle = "car"
+console.log(`is vehicle !== car`);
+console.log(_vehicle !== "car");
+
+const price = 24000
+console.log(`is price > 21000`)
+console.log(price > 20000);
